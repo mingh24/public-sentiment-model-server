6b9a9eac68c16e7d84cd9f2c453dc2ca5d28d5f4
mingh24 <1275432232@qq.com>
2022-04-15 10:48:07 +0800

    feat: 获取亲密同学观点分布时null检查逻辑

--- src/main/java/com/yi/psms/dao/QuestionNodeRepository.java
+++ src/main/java/com/yi/psms/dao/QuestionNodeRepository.java
@@ -31,7 +31,7 @@ public interface QuestionNodeRepository extends Neo4jRepository<QuestionNode, Lo
     CompletableFuture<Integer> countByQuestionIdAndLengthOption(@Param("questionId") Integer questionId, @Param("lengthOption") String lengthOption);
 
     @Async
-    @Query("MATCH (s1:Student)-[r:FRIEND|ROOMMATE|CLASSMATE]->(s2:Student) WHERE s1.studentId = $studentId WITH s2, r ORDER BY r.intimacy DESC, s2.studentId ASC MATCH(s2)-[o:OPINION]->(q:Question) WHERE q.questionId = $questionId RETURN {studentId: s2.studentId, relationship: type(r), intimacy: r.intimacy, attitude: o.attitude, priceOption: o.priceOption, lengthOption: o.lengthOption, view: o.view}")
+    @Query("MATCH (s1:Student)-[r:FRIEND|ROOMMATE|CLASSMATE]->(s2:Student) WHERE s1.studentId = $studentId AND r.intimacy IS NOT NULL WITH s2, r ORDER BY r.intimacy DESC, s2.studentId ASC MATCH(s2)-[o:OPINION]->(q:Question) WHERE q.questionId = $questionId RETURN {studentId: s2.studentId, relationship: type(r), intimacy: r.intimacy, attitude: o.attitude, priceOption: o.priceOption, lengthOption: o.lengthOption, view: o.view}")
     CompletableFuture<List<MapValue>> getIntimateOpinionByStudentIdAndQuestionId(@Param("studentId") Integer studentId, @Param("questionId") Integer questionId);
 
 }
